9bf6804aa7bd15679aec2532d0f38a5308ee87d1
Tiago Lima <tiago_limao_@hotmail.com>
2018-03-28 17:13:14 -0300

    Adicionado excexoes

--- xadresprojeto/Program.cs
+++ xadresprojeto/Program.cs
@@ -14,13 +14,13 @@ namespace xadresprojeto
         {
             Tabuleiro tab = new Tabuleiro(8,8);
 
+          
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
+                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                Tela.imprimirTabuleiro(tab);
 
-             tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 7));
-            tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
-            Tela.imprimirTabuleiro(tab);
-
-            Console.WriteLine("asd".PadLeft(16));
+          
             Console.ReadLine();
 
             
