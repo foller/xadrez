793e83fd06810f1823364480792997255bea6295
Tiago Lima <tiago_limao_@hotmail.com>
2018-04-03 15:36:02 -0300

    Finalizado

--- xadresprojeto/xadres/PartidaDeXadres.cs
+++ xadresprojeto/xadres/PartidaDeXadres.cs
@@ -147,6 +147,21 @@ namespace xadres
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Voce nao pode se colocar em cheque");
             }
+
+            Peca p = tab.peca(destino);
+
+            // jogada especial promocao
+            if( p is Peao){
+                if((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7) ){
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+
+            }
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -161,7 +176,8 @@ namespace xadres
             turno++;
             mudaJogador();
                 }
-            Peca p = tab.peca(destino);
+
+            
             //Jogada especial # en passant
             if(p is Peao &&  (destino.linha ==  origem.linha - 2 ||  destino.linha  ==  origem.linha + 2)){
                 vulneravelEnPassant = p;    
